ac1102a7a205be2f7082e3132cba84484ddcbdae
Martin Dufour <sonic99@live.ca>
2019-11-20 10:51:36 -0500

    Camera changed

---
+++ Kama/Assets/Source/Scripts/CharXMoves/Camera/ThirdPersonCameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    public float rotationSpeed = 1;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+
+    public Transform Obstruction;
+    float zoomSpeed = 2f;
+
+    private void Start()
+    {
+        Obstruction = Target;
+        Player = PlayerManager.instance.player.transform;   
+    }
+    void LateUpdate()
+    {
+        CamControl();
+    }
+
+    private void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(Target);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        }
+        else
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+
+    }
+
+    private void ViewObstructed()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Target.position - transform.position, out hit, 4.5f))
+        {
+            if (hit.collider.gameObject.tag != "Main Character")
+            {
+                Obstruction = hit.transform;
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
